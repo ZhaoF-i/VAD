f8b4e7ea665d8534576e273785813ea3ddd398ee
Zhaofei <1281041324@qq.com>
2021-11-30 00:42:13 +0800

    进行了 1 single channel mel 2 multi channel complex

---
+++ utils/split_eval_train.py
@@ -0,0 +1,16 @@
+import random
+
+import path
+import numpy as np
+import os
+from pathlib import Path
+if __name__ == '__main__':
+    # name_lst=list(Path("/data01/spj/ai_shell4_vad/TRAIN/seg_wav/").rglob('*.wav'))
+    name_lst=list(Path("/data01/spj/asr_dataset/ai_shell4_vad/TRAIN/seg_wav/").rglob('*.wav'))
+    random.shuffle(name_lst)
+    len_lst=len(name_lst)
+    train_lst=name_lst[:int(0.9*len_lst)]
+    eval_lst=name_lst[int(0.9*len_lst):]
+    np.save("../validate.npy",eval_lst)
+    np.save("../train.npy",train_lst)
+    print("列表已经生成")
